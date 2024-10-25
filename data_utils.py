7cfa05e3102dd7b6d347e2970fde3d6f6ec949d3
Zhuofan Shen <shenzhuofan01@gmail.com>
2024-10-25 14:52:40 -0400

    Add files via upload

---
+++ data_utils.py
@@ -0,0 +1,392 @@
+from __future__ import print_function
+
+import numpy as np
+import torch
+import torch.utils
+from prody import *
+
+
+confProDy(verbosity="none")
+
+element_list = [
+    "H",
+    "He",
+    "Li",
+    "Be",
+    "B",
+    "C",
+    "N",
+    "O",
+    "F",
+    "Ne",
+    "Na",
+    "Mg",
+    "Al",
+    "Si",
+    "P",
+    "S",
+    "Cl",
+    "Ar",
+    "K",
+    "Ca",
+    "Sc",
+    "Ti",
+    "V",
+    "Cr",
+    "Mn",
+    "Fe",
+    "Co",
+    "Ni",
+    "Cu",
+    "Zn",
+    "Ga",
+    "Ge",
+    "As",
+    "Se",
+    "Br",
+    "Kr",
+    "Rb",
+    "Sr",
+    "Y",
+    "Zr",
+    "Nb",
+    "Mb",
+    "Tc",
+    "Ru",
+    "Rh",
+    "Pd",
+    "Ag",
+    "Cd",
+    "In",
+    "Sn",
+    "Sb",
+    "Te",
+    "I",
+    "Xe",
+    "Cs",
+    "Ba",
+    "La",
+    "Ce",
+    "Pr",
+    "Nd",
+    "Pm",
+    "Sm",
+    "Eu",
+    "Gd",
+    "Tb",
+    "Dy",
+    "Ho",
+    "Er",
+    "Tm",
+    "Yb",
+    "Lu",
+    "Hf",
+    "Ta",
+    "W",
+    "Re",
+    "Os",
+    "Ir",
+    "Pt",
+    "Au",
+    "Hg",
+    "Tl",
+    "Pb",
+    "Bi",
+    "Po",
+    "At",
+    "Rn",
+    "Fr",
+    "Ra",
+    "Ac",
+    "Th",
+    "Pa",
+    "U",
+    "Np",
+    "Pu",
+    "Am",
+    "Cm",
+    "Bk",
+    "Cf",
+    "Es",
+    "Fm",
+    "Md",
+    "No",
+    "Lr",
+    "Rf",
+    "Db",
+    "Sg",
+    "Bh",
+    "Hs",
+    "Mt",
+    "Ds",
+    "Rg",
+    "Cn",
+    "Uut",
+    "Fl",
+    "Uup",
+    "Lv",
+    "Uus",
+    "Uuo",
+]
+element_list = [item.upper() for item in element_list]
+element_dict = dict(zip(element_list, range(1, len(element_list))))
+
+restype_3to1 = {
+    "ALA": "A",
+    "ARG": "R",
+    "ASN": "N",
+    "ASP": "D",
+    "CYS": "C",
+    "GLN": "Q",
+    "GLU": "E",
+    "GLY": "G",
+    "HIS": "H",
+    "ILE": "I",
+    "LEU": "L",
+    "LYS": "K",
+    "MET": "M",
+    "PHE": "F",
+    "PRO": "P",
+    "SER": "S",
+    "THR": "T",
+    "TRP": "W",
+    "TYR": "Y",
+    "VAL": "V",
+}
+
+atom_order = {
+    "N": 0,
+    "CA": 1,
+    "C": 2,
+    "CB": 3,
+    "O": 4,
+    "CG": 5,
+    "CG1": 6,
+    "CG2": 7,
+    "OG": 8,
+    "OG1": 9,
+    "SG": 10,
+    "CD": 11,
+    "CD1": 12,
+    "CD2": 13,
+    "ND1": 14,
+    "ND2": 15,
+    "OD1": 16,
+    "OD2": 17,
+    "SD": 18,
+    "CE": 19,
+    "CE1": 20,
+    "CE2": 21,
+    "CE3": 22,
+    "NE": 23,
+    "NE1": 24,
+    "NE2": 25,
+    "OE1": 26,
+    "OE2": 27,
+    "CH2": 28,
+    "NH1": 29,
+    "NH2": 30,
+    "OH": 31,
+    "CZ": 32,
+    "CZ2": 33,
+    "CZ3": 34,
+    "NZ": 35,
+    "OXT": 36,
+}
+
+atom_types_4 = ["N", "CA", "C", "O"]
+
+atom_types_all = [
+    "N",
+    "CA",
+    "C",
+    "CB",
+    "O",
+    "CG",
+    "CG1",
+    "CG2",
+    "OG",
+    "OG1",
+    "SG",
+    "CD",
+    "CD1",
+    "CD2",
+    "ND1",
+    "ND2",
+    "OD1",
+    "OD2",
+    "SD",
+    "CE",
+    "CE1",
+    "CE2",
+    "CE3",
+    "NE",
+    "NE1",
+    "NE2",
+    "OE1",
+    "OE2",
+    "CH2",
+    "NH1",
+    "NH2",
+    "OH",
+    "CZ",
+    "CZ2",
+    "CZ3",
+    "NZ",
+]
+
+
+def get_aligned_coordinates(protein_atoms, CA_dict: dict, atom_name: str):
+    """
+    protein_atoms: prody atom group
+    CA_dict: mapping between chain_residue_idx_icodes and integers
+    atom_name: atom to be parsed; e.g. CA
+    """
+    atom_atoms = protein_atoms.select(f"name {atom_name}")
+
+    if atom_atoms != None:
+        atom_coords = atom_atoms.getCoords()
+        atom_resnums = atom_atoms.getResnums()
+        atom_chain_ids = atom_atoms.getChids()
+        atom_icodes = atom_atoms.getIcodes()
+
+    atom_coords_ = np.zeros([len(CA_dict), 3], np.float32)
+    atom_coords_m = np.zeros([len(CA_dict)], np.int32)
+    if atom_atoms != None:
+        for i in range(len(atom_resnums)):
+            code = atom_chain_ids[i] + "_" + str(atom_resnums[i]) + "_" + atom_icodes[i]
+            if code in list(CA_dict):
+                atom_coords_[CA_dict[code], :] = atom_coords[i]
+                atom_coords_m[CA_dict[code]] = 1
+    return atom_coords_, atom_coords_m
+
+def parse_PDB(
+    input_path: str,
+    device: str = "cpu",
+    chains: list = [],
+    parse_all_atoms: bool = False,
+    parse_atoms_with_zero_occupancy: bool = False
+):
+    """
+    input_path : path for the input PDB
+    device: device for the torch.Tensor
+    chains: a list specifying which chains need to be parsed; e.g. ["A", "B"]
+    parse_all_atoms: if False parse only N,CA,C,O otherwise all 37 atoms
+    parse_atoms_with_zero_occupancy: if True atoms with zero occupancy will be parsed
+    """
+
+    if not parse_all_atoms:
+        atom_types = atom_types_4
+    else:
+        atom_types = atom_types_all
+    atoms = parsePDB(input_path)
+    if not parse_atoms_with_zero_occupancy:
+        atoms = atoms.select("occupancy > 0")
+    if chains:
+        str_out = ""
+        for item in chains:
+            str_out += " chain " + item + " or"
+        atoms = atoms.select(str_out[1:-3])
+
+    protein_atoms = atoms.select("protein")
+    backbone = protein_atoms.select("backbone")
+    other_atoms = atoms.select("not protein and not water")
+    water_atoms = atoms.select("water")
+
+    CA_atoms = protein_atoms.select("name CA")
+    CA_resnums = CA_atoms.getResnums()
+    CA_chain_ids = CA_atoms.getChids()
+    CA_icodes = CA_atoms.getIcodes()
+
+    CA_dict = {}
+    for i in range(len(CA_resnums)):
+        code = CA_chain_ids[i] + "_" + str(CA_resnums[i]) + "_" + CA_icodes[i]
+        CA_dict[code] = i
+
+    xyz_37 = np.zeros([len(CA_dict), 37, 3], np.float32)
+    xyz_37_m = np.zeros([len(CA_dict), 37], np.int32)
+    for atom_name in atom_types:
+        xyz, xyz_m = get_aligned_coordinates(protein_atoms, CA_dict, atom_name)
+        xyz_37[:, atom_order[atom_name], :] = xyz
+        xyz_37_m[:, atom_order[atom_name]] = xyz_m
+
+    N = xyz_37[:, atom_order["N"], :]
+    CA = xyz_37[:, atom_order["CA"], :]
+    C = xyz_37[:, atom_order["C"], :]
+    O = xyz_37[:, atom_order["O"], :]
+
+    N_m = xyz_37_m[:, atom_order["N"]]
+    CA_m = xyz_37_m[:, atom_order["CA"]]
+    C_m = xyz_37_m[:, atom_order["C"]]
+    O_m = xyz_37_m[:, atom_order["O"]]
+
+    mask = N_m * CA_m * C_m * O_m  # must all 4 atoms exist
+
+    b = CA - N
+    c = C - CA
+    a = np.cross(b, c, axis=-1)
+    CB = -0.58273431 * a + 0.56802827 * b - 0.54067466 * c + CA
+
+    chain_labels = np.array(CA_atoms.getChindices(), dtype=np.int32)
+    R_idx = np.array(CA_resnums, dtype=np.int32)
+    S = CA_atoms.getResnames()
+    S = [restype_3to1[AA] if AA in list(restype_3to1) else "X" for AA in list(S)]
+    S = np.array(["ACDEFGHIKLMNPQRSTVWYX".find(AA) for AA in list(S)], np.int32) # restype_STRtoINT[AA]
+    X = np.concatenate([N[:, None], CA[:, None], C[:, None], O[:, None]], 1)
+
+    try:
+        Y = np.array(other_atoms.getCoords(), dtype=np.float32)
+        Y_t = list(other_atoms.getElements())
+        Y_t = np.array(
+            [
+                element_dict[y_t.upper()] if y_t.upper() in element_list else 0
+                for y_t in Y_t
+            ],
+            dtype=np.int32,
+        )
+        Y_m = (Y_t != 1) * (Y_t != 0)
+
+        Y = Y[Y_m, :]
+        Y_t = Y_t[Y_m]
+        Y_m = Y_m[Y_m]
+    except:
+        print("Error loading ligand atoms.")
+        Y = np.zeros([1, 3], np.float32)
+        Y_t = np.zeros([1], np.int32)
+        Y_m = np.zeros([1], np.int32)
+
+    output_dict = {}
+    output_dict["X"] = torch.tensor(X, device=device, dtype=torch.float32)
+    output_dict["mask"] = torch.tensor(mask, device=device, dtype=torch.int32)
+    output_dict["Y"] = torch.tensor(Y, device=device, dtype=torch.float32)
+    output_dict["Y_t"] = torch.tensor(Y_t, device=device, dtype=torch.int32)
+    output_dict["Y_m"] = torch.tensor(Y_m, device=device, dtype=torch.int32)
+
+    output_dict["R_idx"] = torch.tensor(R_idx, device=device, dtype=torch.int32)
+    output_dict["chain_labels"] = torch.tensor(
+        chain_labels, device=device, dtype=torch.int32
+    )
+
+    output_dict["chain_letters"] = CA_chain_ids
+
+    mask_c = []
+    chain_list = list(set(output_dict["chain_letters"]))
+    chain_list.sort()
+    for chain in chain_list:
+        mask_c.append(
+            torch.tensor(
+                [chain == item for item in output_dict["chain_letters"]],
+                device=device,
+                dtype=bool,
+            )
+        )
+
+    output_dict["mask_c"] = mask_c
+    output_dict["chain_list"] = chain_list
+
+    output_dict["S"] = torch.tensor(S, device=device, dtype=torch.int32)
+
+    output_dict["xyz_37"] = torch.tensor(xyz_37, device=device, dtype=torch.float32)
+    output_dict["xyz_37_m"] = torch.tensor(xyz_37_m, device=device, dtype=torch.int32)
+
+    return output_dict, backbone, other_atoms, CA_icodes, water_atoms
---
+++ model_data_utils.py
@@ -0,0 +1,537 @@
+import json, time, os, sys, glob
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class StructureDataset():
+    def __init__(self, jsonl_file, verbose=True, truncate=None, max_length=100,
+        alphabet='ACDEFGHIKLMNPQRSTVWYX-'):
+        alphabet_set = set([a for a in alphabet])
+        discard_count = {
+            'bad_chars': 0,
+            'too_long': 0,
+            'bad_seq_length': 0
+        }
+
+        with open(jsonl_file) as f:
+            self.data = []
+
+            lines = f.readlines()
+            start = time.time()
+            for i, line in enumerate(lines):
+                entry = json.loads(line)
+                seq = entry['seq'] 
+                name = entry['name']
+
+                # Convert raw coords to np arrays
+                #for key, val in entry['coords'].items():
+                #    entry['coords'][key] = np.asarray(val)
+
+                # Check if in alphabet
+                bad_chars = set([s for s in seq]).difference(alphabet_set)
+                if len(bad_chars) == 0:
+                    if len(entry['seq']) <= max_length:
+                        if True:
+                            self.data.append(entry)
+                        else:
+                            discard_count['bad_seq_length'] += 1
+                    else:
+                        discard_count['too_long'] += 1
+                else:
+                    print(name, bad_chars, entry['seq'])
+                    discard_count['bad_chars'] += 1
+
+                # Truncate early
+                if truncate is not None and len(self.data) == truncate:
+                    return
+
+                if verbose and (i + 1) % 1000 == 0:
+                    elapsed = time.time() - start
+                    print('{} entries ({} loaded) in {:.1f} s'.format(len(self.data), i+1, elapsed))
+
+            print('discarded', discard_count)
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+class StructureDatasetPDB():
+    def __init__(self, pdb_dict_list, verbose=True, truncate=None, max_length=100,
+        alphabet='ACDEFGHIKLMNPQRSTVWYX-'):
+        alphabet_set = set([a for a in alphabet])
+        discard_count = {
+            'bad_chars': 0,
+            'too_long': 0,
+            'bad_seq_length': 0
+        }
+
+        self.data = []
+
+        start = time.time()
+        for i, entry in enumerate(pdb_dict_list):
+            # print(entry)
+            # print(entry.keys())
+            seq = entry['S']
+            # name = entry['name']
+
+            # bad_chars = set([s for s in seq]).difference(alphabet_set)
+            # if len(bad_chars) == 0:
+            if seq.shape[0] <= max_length:
+                self.data.append(entry)
+            else:
+                discard_count['too_long'] += 1
+            # else:
+            #     #print(name, bad_chars, entry['seq'])
+            #     discard_count['bad_chars'] += 1
+
+            # Truncate early
+            if truncate is not None and len(self.data) == truncate:
+                return
+
+            if verbose and (i + 1) % 1000 == 0:
+                elapsed = time.time() - start
+                #print('{} entries ({} loaded) in {:.1f} s'.format(len(self.data), i+1, elapsed))
+
+            #print('Discarded', discard_count)
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+    
+class StructureLoader():
+    def __init__(self, dataset, batch_size=100, shuffle=True,
+        collate_fn=lambda x:x, drop_last=False):
+        self.dataset = dataset
+        self.size = len(dataset)
+        self.lengths = [len(dataset[i]['seq']) for i in range(self.size)]
+        self.batch_size = batch_size
+        sorted_ix = np.argsort(self.lengths)
+
+        # Cluster into batches of similar sizes
+        clusters, batch = [], []
+        batch_max = 0
+        for ix in sorted_ix:
+            size = self.lengths[ix]
+            if size * (len(batch) + 1) <= self.batch_size:
+                batch.append(ix)
+                batch_max = size
+            else:
+                clusters.append(batch)
+                batch, batch_max = [], 0
+        if len(batch) > 0:
+            clusters.append(batch)
+        self.clusters = clusters
+
+    def __len__(self):
+        return len(self.clusters)
+
+    def __iter__(self):
+        np.random.shuffle(self.clusters)
+        for b_idx in self.clusters:
+            batch = [self.dataset[i] for i in b_idx]
+            yield batch
+
+
+side_chain_atom_types = [
+    6,
+    6,
+    6,
+    8,
+    8,
+    16,
+    6,
+    6,
+    6,
+    7,
+    7,
+    8,
+    8,
+    16,
+    6,
+    6,
+    6,
+    6,
+    7,
+    7,
+    7,
+    8,
+    8,
+    6,
+    7,
+    7,
+    8,
+    6,
+    6,
+    6,
+    7,
+    8,
+]
+
+periodic_table_features = [
+    [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
+        21,
+        22,
+        23,
+        24,
+        25,
+        26,
+        27,
+        28,
+        29,
+        30,
+        31,
+        32,
+        33,
+        34,
+        35,
+        36,
+        37,
+        38,
+        39,
+        40,
+        41,
+        42,
+        43,
+        44,
+        45,
+        46,
+        47,
+        48,
+        49,
+        50,
+        51,
+        52,
+        53,
+        54,
+        55,
+        56,
+        57,
+        58,
+        59,
+        60,
+        61,
+        62,
+        63,
+        64,
+        65,
+        66,
+        67,
+        68,
+        69,
+        70,
+        71,
+        72,
+        73,
+        74,
+        75,
+        76,
+        77,
+        78,
+        79,
+        80,
+        81,
+        82,
+        83,
+        84,
+        85,
+        86,
+        87,
+        88,
+        89,
+        90,
+        91,
+        92,
+        93,
+        94,
+        95,
+        96,
+        97,
+        98,
+        99,
+        100,
+        101,
+        102,
+        103,
+        104,
+        105,
+        106,
+        107,
+        108,
+        109,
+        110,
+        111,
+        112,
+        113,
+        114,
+        115,
+        116,
+        117,
+        118,
+    ],
+    [
+        0,
+        1,
+        18,
+        1,
+        2,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        1,
+        2,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        1,
+        2,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        1,
+        2,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+    ],
+    [
+        0,
+        1,
+        1,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        6,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+        7,
+    ],
+]
