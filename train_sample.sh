e2873ab9c59812b62fbf9a09551d94e3fb2b78b6
Zhuofan Shen <shenzhuofan01@gmail.com>
2024-11-05 13:26:43 -0500

    Update train_sample.sh

--- train_sample.sh
+++ train_sample.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#SBATCH --clusters=amarel
 #SBATCH --partition=gpu
 #SBATCH --job-name=train
 #SBATCH --gres=gpu:1
@@ -14,8 +13,9 @@
 #SBATCH --open-mode=append
 
 python ./train.py \
+       --model_type "ligand_mpnn_new" \
        --cpus_per_task 12 \
        --path_for_outputs "./exp_020" \
        --path_for_training_data "sample" \
-       --num_examples_per_epoch 100 \
+       --num_examples_per_epoch 50 \
        --save_model_every_n_epochs 50
