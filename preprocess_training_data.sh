314c74a6cc80712e6ec3fc646277bcd84700075c
Zhuofan Shen <shenzhuofan01@gmail.com>
2024-11-08 17:35:30 -0500

    store training data in CPU memory
    Store training data in CPU memory. During each training epoch, change the batch data device to CUDA on-the-fly.

--- preprocess_training_data.sh
+++ preprocess_training_data.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#SBATCH --clusters=amarel
-#SBATCH --partition=gpu
-#SBATCH --job-name=binarize
-#SBATCH --gres=gpu:1
+#SBATCH --partition=main
+#SBATCH --job-name=preprocess_dataset
 #SBATCH --cpus-per-task 1
 #SBATCH --mem=3000
 #SBATCH --time=3-00:00:00
@@ -13,6 +11,6 @@
 #SBATCH --begin=now
 #SBATCH --open-mode=append
 
-python preprocess_training_data.py training/train.json
-python preprocess_training_data.py training/valid.json
+python preprocess_training_data.py ${1}/train.json
+python preprocess_training_data.py ${1}/valid.json
 
