0415070198562bfa3525a685bbc00ffc954b6e7a
Zhuofan Shen <shenzhuofan01@gmail.com>
2024-11-08 17:41:52 -0500

    store training data in CPU memory
    Store training data in CPU memory. During each training epoch, change the batch data device to CUDA on-the-fly.

--- train.py
+++ train.py
@@ -15,8 +15,10 @@ from model_data_utils import PDBDataset, load_pdb_pt, worker_init_fn, \
 def main(args):
     if args.model_type == "ligand_mpnn_new":
         from model_protein2ligand_MPNN import loss_smoothed, loss_nll, ProteinMPNN
-    else:
+    elif args.model_type == "ligand_mpnn":
         from model import loss_smoothed, loss_nll, ProteinMPNN
+    elif args.model_type == "protein_mpnn":
+        from model_ProteinMPNN import loss_smoothed, loss_nll, ProteinMPNN
 
     scaler = GradScaler()
     device = torch.device("cuda" if (torch.cuda.is_available()) else "cpu")
@@ -80,123 +82,123 @@ def main(args):
         optimizer.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
     torch.multiprocessing.set_start_method('spawn')
-    executor = ProcessPoolExecutor(max_workers=args.cpus_per_task)
-    q = queue.Queue(maxsize=3)
-    p = queue.Queue(maxsize=3)
-    for _ in range(3):
-        q.put_nowait(executor.submit(get_pdbs, train_loader, args.max_protein_length, args.num_examples_per_epoch))
-        p.put_nowait(executor.submit(get_pdbs, valid_loader, args.max_protein_length, args.num_examples_per_epoch))
+    with ProcessPoolExecutor(max_workers=args.cpus_per_task) as executor:
+        q = queue.Queue(maxsize=3)
+        p = queue.Queue(maxsize=3)
+        for _ in range(3):
+            q.put_nowait(executor.submit(get_pdbs, train_loader, args.max_protein_length, args.num_examples_per_epoch))
+            p.put_nowait(executor.submit(get_pdbs, valid_loader, args.max_protein_length, args.num_examples_per_epoch))
 
-    for e in range(args.num_epochs):
-        t0 = time.time()
-        e = epoch + e
-        model.train()
-        train_sum, train_weights = 0., 0.
-        train_acc = 0.
+        for e in range(args.num_epochs):
+            t0 = time.time()
+            e = epoch + e
+            model.train()
+            train_sum, train_weights = 0., 0.
+            train_acc = 0.
 
-        if e % args.reload_data_every_n_epochs == 0:
-            pdb_dict_train = q.get().result()
-            pdb_dict_valid = p.get().result()
-            train_batch_list = Batches(pdb_dict_train, max_length=args.max_protein_length, batch_size=args.batch_size)
-            valid_batch_list = Batches(pdb_dict_valid, max_length=args.max_protein_length, batch_size=args.batch_size)
-            if e > epoch:
-                q.put_nowait(executor.submit(get_pdbs, train_loader, args.max_protein_length, args.num_examples_per_epoch))
-                p.put_nowait(executor.submit(get_pdbs, valid_loader, args.max_protein_length, args.num_examples_per_epoch))
+            if e % args.reload_data_every_n_epochs == 0:
+                pdb_dict_train = q.get().result()
+                pdb_dict_valid = p.get().result()
+                train_batch_list = Batches(pdb_dict_train, max_length=args.max_protein_length, batch_size=args.batch_size)
+                valid_batch_list = Batches(pdb_dict_valid, max_length=args.max_protein_length, batch_size=args.batch_size)
+                if e > epoch:
+                    q.put_nowait(executor.submit(get_pdbs, train_loader, args.max_protein_length, args.num_examples_per_epoch))
+                    p.put_nowait(executor.submit(get_pdbs, valid_loader, args.max_protein_length, args.num_examples_per_epoch))
 
-        for batch in train_batch_list:
-            start_batch = time.time()
-            batch_feature_dict = batch_featurize(batch, device)
-            elapsed_featurize = time.time() - start_batch
-            optimizer.zero_grad()
-            S = batch_feature_dict["S"]
-            mask_for_loss = batch_feature_dict["mask"]
-            # mask_for_loss = mask*chain_M
-            
-            if args.mixed_precision:
-                with torch.cuda.amp.autocast():
-                    log_probs = model(batch_feature_dict)
-                    _, loss_av_smoothed = loss_smoothed(S, log_probs, mask_for_loss)
-
-                scaler.scale(loss_av_smoothed).backward()
-
-                if args.gradient_norm > 0.0:
-                    total_norm = clip_grad_norm_(model.parameters(), args.gradient_norm)
-
-                scaler.step(optimizer)
-                scaler.update()
-            else:
-                log_probs = model(batch_feature_dict)
-                _, loss_av_smoothed = loss_smoothed(S, log_probs, mask_for_loss)
-                loss_av_smoothed.backward()
-
-                if args.gradient_norm > 0.0:
-                    total_norm = clip_grad_norm_(model.parameters(), args.gradient_norm)
-
-                optimizer.step()
-
-            loss, loss_av, true_false = loss_nll(S, log_probs, mask_for_loss)
-        
-            train_sum += torch.sum(loss * mask_for_loss).cpu().data.numpy()
-            train_acc += torch.sum(true_false * mask_for_loss).cpu().data.numpy()
-            train_weights += torch.sum(mask_for_loss).cpu().data.numpy()
-
-            total_step += 1
-
-        model.eval()
-        with torch.no_grad():
-            validation_sum, validation_weights = 0., 0.
-            validation_acc = 0.
-            for batch in valid_batch_list:
+            for batch in train_batch_list:
+                start_batch = time.time()
                 batch_feature_dict = batch_featurize(batch, device)
-                log_probs = model(batch_feature_dict)
+                elapsed_featurize = time.time() - start_batch
+                optimizer.zero_grad()
                 S = batch_feature_dict["S"]
                 mask_for_loss = batch_feature_dict["mask"]
-                loss, loss_av, true_false = loss_nll(S, log_probs, mask_for_loss)
+                # mask_for_loss = mask*chain_M
                 
-                validation_sum += torch.sum(loss * mask_for_loss).cpu().data.numpy()
-                validation_acc += torch.sum(true_false * mask_for_loss).cpu().data.numpy()
-                validation_weights += torch.sum(mask_for_loss).cpu().data.numpy()
-        
-        train_loss = train_sum / train_weights
-        train_accuracy = train_acc / train_weights
-        train_perplexity = np.exp(train_loss)
-        validation_loss = validation_sum / validation_weights
-        validation_accuracy = validation_acc / validation_weights
-        validation_perplexity = np.exp(validation_loss)
-        
-        train_perplexity_ = np.format_float_positional(np.float32(train_perplexity), unique=False, precision=3)     
-        validation_perplexity_ = np.format_float_positional(np.float32(validation_perplexity), unique=False, precision=3)
-        train_accuracy_ = np.format_float_positional(np.float32(train_accuracy), unique=False, precision=3)
-        validation_accuracy_ = np.format_float_positional(np.float32(validation_accuracy), unique=False, precision=3)
+                if args.mixed_precision:
+                    with torch.cuda.amp.autocast():
+                        log_probs = model(batch_feature_dict)
+                        _, loss_av_smoothed = loss_smoothed(S, log_probs, mask_for_loss)
 
-        t1 = time.time()
-        dt = np.format_float_positional(np.float32(t1-t0), unique=False, precision=1) 
-        with open(logfile, 'a') as f:
-            f.write(f'epoch: {e+1}, step: {total_step}, time: {dt}, train: {train_perplexity_}, valid: {validation_perplexity_}, train_acc: {train_accuracy_}, valid_acc: {validation_accuracy_}\n')
-        print(f'epoch: {e+1}, step: {total_step}, time: {dt}, train: {train_perplexity_}, valid: {validation_perplexity_}, train_acc: {train_accuracy_}, valid_acc: {validation_accuracy_}')
-        
-        checkpoint_filename_last = os.path.join(base_folder, 'model_weights', 'epoch_last.pt')
-        torch.save({
-                    'epoch': e+1,
-                    'step': total_step,
-                    'num_edges' : args.num_neighbors,
-                    'atom_context_num': args.atom_context_num,
-                    'noise_level': args.backbone_noise,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': optimizer.optimizer.state_dict(),
-                    }, checkpoint_filename_last)
+                    scaler.scale(loss_av_smoothed).backward()
 
-        if (e+1) % args.save_model_every_n_epochs == 0:
-            checkpoint_filename = os.path.join(base_folder, 'model_weights', 'epoch{}_step{}.pt'.format(e+1, total_step))
+                    if args.gradient_norm > 0.0:
+                        total_norm = clip_grad_norm_(model.parameters(), args.gradient_norm)
+
+                    scaler.step(optimizer)
+                    scaler.update()
+                else:
+                    log_probs = model(batch_feature_dict)
+                    _, loss_av_smoothed = loss_smoothed(S, log_probs, mask_for_loss)
+                    loss_av_smoothed.backward()
+
+                    if args.gradient_norm > 0.0:
+                        total_norm = clip_grad_norm_(model.parameters(), args.gradient_norm)
+
+                    optimizer.step()
+
+                loss, loss_av, true_false = loss_nll(S, log_probs, mask_for_loss)
+            
+                train_sum += torch.sum(loss * mask_for_loss).cpu().data.numpy()
+                train_acc += torch.sum(true_false * mask_for_loss).cpu().data.numpy()
+                train_weights += torch.sum(mask_for_loss).cpu().data.numpy()
+
+                total_step += 1
+
+            model.eval()
+            with torch.no_grad():
+                validation_sum, validation_weights = 0., 0.
+                validation_acc = 0.
+                for batch in valid_batch_list:
+                    batch_feature_dict = batch_featurize(batch, device)
+                    log_probs = model(batch_feature_dict)
+                    S = batch_feature_dict["S"]
+                    mask_for_loss = batch_feature_dict["mask"]
+                    loss, loss_av, true_false = loss_nll(S, log_probs, mask_for_loss)
+                    
+                    validation_sum += torch.sum(loss * mask_for_loss).cpu().data.numpy()
+                    validation_acc += torch.sum(true_false * mask_for_loss).cpu().data.numpy()
+                    validation_weights += torch.sum(mask_for_loss).cpu().data.numpy()
+            
+            train_loss = train_sum / train_weights
+            train_accuracy = train_acc / train_weights
+            train_perplexity = np.exp(train_loss)
+            validation_loss = validation_sum / validation_weights
+            validation_accuracy = validation_acc / validation_weights
+            validation_perplexity = np.exp(validation_loss)
+            
+            train_perplexity_ = np.format_float_positional(np.float32(train_perplexity), unique=False, precision=3)     
+            validation_perplexity_ = np.format_float_positional(np.float32(validation_perplexity), unique=False, precision=3)
+            train_accuracy_ = np.format_float_positional(np.float32(train_accuracy), unique=False, precision=3)
+            validation_accuracy_ = np.format_float_positional(np.float32(validation_accuracy), unique=False, precision=3)
+
+            t1 = time.time()
+            dt = np.format_float_positional(np.float32(t1-t0), unique=False, precision=1) 
+            with open(logfile, 'a') as f:
+                f.write(f'epoch: {e+1}, step: {total_step}, time: {dt}, train: {train_perplexity_}, valid: {validation_perplexity_}, train_acc: {train_accuracy_}, valid_acc: {validation_accuracy_}\n')
+            print(f'epoch: {e+1}, step: {total_step}, time: {dt}, train: {train_perplexity_}, valid: {validation_perplexity_}, train_acc: {train_accuracy_}, valid_acc: {validation_accuracy_}')
+            
+            checkpoint_filename_last = os.path.join(base_folder, 'model_weights', 'epoch_last.pt')
             torch.save({
-                    'epoch': e+1,
-                    'step': total_step,
-                    'num_edges' : args.num_neighbors,
-                    'atom_context_num': args.atom_context_num,
-                    'noise_level': args.backbone_noise, 
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': optimizer.optimizer.state_dict(),
-                    }, checkpoint_filename)
+                        'epoch': e+1,
+                        'step': total_step,
+                        'num_edges' : args.num_neighbors,
+                        'atom_context_num': args.atom_context_num,
+                        'noise_level': args.backbone_noise,
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.optimizer.state_dict(),
+                        }, checkpoint_filename_last)
+
+            if (e+1) % args.save_model_every_n_epochs == 0:
+                checkpoint_filename = os.path.join(base_folder, 'model_weights', 'epoch{}_step{}.pt'.format(e+1, total_step))
+                torch.save({
+                        'epoch': e+1,
+                        'step': total_step,
+                        'num_edges' : args.num_neighbors,
+                        'atom_context_num': args.atom_context_num,
+                        'noise_level': args.backbone_noise, 
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.optimizer.state_dict(),
+                        }, checkpoint_filename)
 
 
 if __name__ == "__main__":
@@ -216,7 +218,7 @@ if __name__ == "__main__":
     argparser.add_argument("--num_encoder_layers", type=int, default=3, help="number of encoder layers")
     argparser.add_argument("--num_decoder_layers", type=int, default=3, help="number of decoder layers")
     argparser.add_argument("--num_neighbors", type=int, default=48, help="number of neighbors for the sparse graph")
-    argparser.add_argument("--model_type", type=str, choices=["protein_mpnn", "ligand_mpnn", "ligand_mpnn_new", "soluble_mpnn"], default="ligand_mpnn_new", help="model type")
+    argparser.add_argument("--model_type", type=str, choices=["protein_mpnn", "ligand_mpnn", "ligand_mpnn_new"], default="ligand_mpnn_new", help="model type")
     argparser.add_argument("--atom_context_num", type=int, default=30, help="number of context atom neighbors for the sparse graph")
     argparser.add_argument("--dropout", type=float, default=0.1, help="dropout level; 0.0 means no dropout")
     argparser.add_argument("--backbone_noise", type=float, default=0.2, help="amount of noise added to backbone during training")
